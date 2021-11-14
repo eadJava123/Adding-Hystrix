7db4f8f3ebb3558c3802a72da1a87614284f7774
DulatTurganbek <d.turganbek.01@gmail.com>
2021-11-14 22:27:33 +0600

    zuul

---
+++ zuul/src/test/java/iitu/ead4/zuul/ZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package iitu.ead4.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
